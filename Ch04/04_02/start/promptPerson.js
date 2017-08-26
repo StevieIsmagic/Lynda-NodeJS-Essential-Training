8343fafb56f2352bd474d5aa412bba58e2e909e5
Colombianito66 <colombianito66@gmail.com>
2017-08-25 21:11:29 -0400

    progress on user input

--- Ch04/04_02/start/promptPerson.js
+++ Ch04/04_02/start/promptPerson.js
@@ -1,8 +1,22 @@
 var readline = require('readline');
-
 var rl = readline.createInterface(process.stdin, process.stdout);
+realPerson = {
+  name: '',
+  sayings: []
+};
 
 rl.question('What is the name of a real person?', (answer) => {
 
-  console.log(answer);
+  realPerson.name = answer;
+
+  rl.setPrompt(`What would ${realPerson.name} say ? `);
+
+  rl.prompt();
+
+  rl.on('line', (saying) => {
+    
+    rl.setPrompt(`What else would ${realPerson.name} say ? ('exit' to leave) `);
+    
+    rl.prompt()
+  });
 });
