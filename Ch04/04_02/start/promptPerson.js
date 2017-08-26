2b19061b42f28216dc8c05b4dc6824ea2eb0fab2
Colombianito66 <colombianito66@gmail.com>
2017-08-25 21:21:28 -0400

    complete user input functionality

--- Ch04/04_02/start/promptPerson.js
+++ Ch04/04_02/start/promptPerson.js
@@ -1,22 +1,30 @@
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout);
+
 realPerson = {
   name: '',
   sayings: []
 };
 
-rl.question('What is the name of a real person?', (answer) => {
-
+rl.question('What is the name of a real person? ', (answer) => {
   realPerson.name = answer;
-
   rl.setPrompt(`What would ${realPerson.name} say ? `);
-
   rl.prompt();
 
   rl.on('line', (saying) => {
-    
-    rl.setPrompt(`What else would ${realPerson.name} say ? ('exit' to leave) `);
-    
-    rl.prompt()
+
+    realPerson.sayings.push(saying.trim());
+
+    if (saying.toLowerCase().trim() === 'exit') {
+      rl.close();
+    } else {
+      rl.setPrompt(`What else would ${realPerson.name} say ? ('exit' to leave) `);
+      rl.prompt();
+    }
   });
+});
+
+rl.on('close', () => {
+  console.log("%s is a real person that says %j", realPerson.name, realPerson.sayings);
+  process.exit();
 });
